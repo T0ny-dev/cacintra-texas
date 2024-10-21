074506042fbe76f022542720c27f3f876af7e1f4
T0ny-dev <antonioraweb@gmail.com>
2024-10-21 16:12:50 -0600

    update web

--- src/components/MobileNavbar.jsx
+++ src/components/MobileNavbar.jsx
@@ -19,14 +19,9 @@ function MobileNavbar () {
           <div className='menu_mobile_links'>
             <a href="/sobrenosotros">Sobre Nosotros</a>
             <a href="/servicios">Servicios</a>
-            <a href="https://blog.canacintrariobravo.com/">Blog</a>
             <a href="/contacto">Contacto</a>
-            <a href="/sermiembro">Ser Socio</a>
-            <a href="https://canacintrariobravo.com/blog/agenda">Agenda</a>
-            <a href='https://canacintrariobravo.com/blog/wp-admin'>
-              <button className='menu_visible_button'><img src='./globo.png' id='plataformaIcon'/>Iniciar sesion</button>
-            </a>
-            <a href='http://www.plataformaeducativautbc.com/' target={'_blank'}>
+            <a href="/https://cicade.network/">CICADE NETWORK</a>
+            <a href='https://cicade.network/wp-login.php' target={'_blank'}>
               <button className='menu_visible_button'><img src='./plaforma.png' id='plataformaIcon'/>Plataforma Virtual</button>
             </a>
           </div>
